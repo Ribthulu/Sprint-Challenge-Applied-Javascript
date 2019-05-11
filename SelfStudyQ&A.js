e2967d23bfa2614fd012312a15f37cf850257eaf
Ribthulu <myemail@example.com>
2019-05-11 11:54:50 -0400

    set up project and added SelfStudyQ&A.js as my answers to the self study questions

---
+++ SelfStudyQ&A.js
@@ -0,0 +1,13 @@
+// Q&A:
+//
+// Self study questions and answers
+// 1.	What is the DOM?
+// -	It is an object of all the html code that is created by the browser at runtime. It displays all of the code as a JavaScript object to edit all the html elements in real time
+// 2.	What is an event?
+// -	It is something that is enacted in real time, meaning that when the user does a certain command on the web page, that interaction is an event
+// 3.	What is an event listener?
+// -	It is a method that listens to a specific event and is executed in real time as the user interacts with the web page
+// 4.	Why would we convert a NodeList into an Array?
+// -	To be able to use it as an array and loop through all of the array indexes dynamically. Nodelist, depending on what selector is used, targets the dom and is static, whereas targeting the array changes the actual code instead of the dom selector
+// 5.	What is a component?
+// -	It is a set of code that allows certain commands to be reused; it is similar to a library, but it is different due to the ability it has to not impact the entire code, but rather the specific section that will have a high amount of reusability
